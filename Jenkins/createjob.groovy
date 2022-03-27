b556c3d14f6353b6658a33a8200c1311e16efe15
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2022-03-27 08:24:41 +0530

    Update createjob.groovy

--- Jenkins/createjob.groovy
+++ Jenkins/createjob.groovy
@@ -2,4 +2,10 @@ def jobname = 'firstjob'
 
 job(jobname){
   description('First DSL Job')
+  parameters {
+        booleanParam('FLAG', true)
+        choiceParam('SelectTool', ['Jenkins (default)', 'GIT', 'Maven'])
+        stringParam('Name', defaultValue='JenkisnServer', description = 'Learning jenkins')
+        
+    }
 }
