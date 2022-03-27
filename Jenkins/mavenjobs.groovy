6994487576399db03224de239548efd1e4e95a78
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2022-03-27 10:06:18 +0530

    Update mavenjobs.groovy

--- Jenkins/mavenjobs.groovy
+++ Jenkins/mavenjobs.groovy
@@ -1,24 +1,44 @@
-job("maven-compile")
+job("CloneRepo")
     {
       scm{
         git('https://github.com/Sonal0409/DevOpsClassCodes.git')
       }
-      steps{
-        maven('compile')
+      
+      publishers{ 
+        downstream('maven-compile', 'SUCCESS')
       }
+    }
+
+mavenJob("maven-compile")
+    {
+ customWorkspace('/var/lib/jenkins/workspace/CloneRepo')
+        mavenInstallation('Maven 3.8.5')
+        goals('compile')
+        
       publishers{
         
         downstream('maven-test', 'SUCCESS')
       }
     }
     
-    job("maven-test")
+    mavenJob("maven-test")
     {
-      scm{
-        git('https://github.com/Sonal0409/DevOpsClassCodes.git')
-      }
-      steps{
-        maven('test')
+   customWorkspace('/var/lib/jenkins/workspace/CloneRepo')
+        mavenInstallation('Maven 3.8.5')
+       goals('test')
+        
+      publishers{
+        
+        downstream('maven-package', 'SUCCESS')
       }
     
     }
+
+   mavenJob("maven-package")
+    {
+   customWorkspace('/var/lib/jenkins/workspace/CloneRepo')
+        mavenInstallation('Maven 3.8.5')
+       goals('package')
+     
+    }
+
